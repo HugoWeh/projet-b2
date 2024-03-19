165bab3799a7e3de753f27f094a55ea8bdbfb222
HugoWeh <hugo.wehrle@supdevinci-edu.fr>
2024-03-19 14:03:15 +0100

    add Address.jsx

---
+++ src/components/Address.jsx
@@ -0,0 +1,18 @@
+export const Address = () => {
+  return (
+    <div className="flex flex-row m-2 mr-0 items-center">
+      <div className="rounded basis-11/12 border-2 border-black p-2">
+        <h1 className="p-2 text-2xl font-bold">Nos adresses</h1>
+        <p className="text-justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio non
+          consectetur quasi corporis ad illo blanditiis omnis, exercitationem
+          autem, impedit illum! Nulla voluptatem dignissimos nesciunt provident,
+          consectetur saepe mollitia.
+        </p>
+      </div>
+      <p className="bg-green-600 text-white p-4 rounded-r font-semibold">
+        Ouvert
+      </p>
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Nav } from "@/components/Nav";
 import { Stars } from "@/components/Stars";
+import { Address } from "@/components/Address";
 
 const HomePage = () => {
   return (
@@ -19,33 +20,13 @@ const HomePage = () => {
             </ul>
           </div>
         </div>
-        <div className="flex flex-wrap items-center w-full">
-          <div className="rounded basis-11/12 border-2 border-black m-2 mr-0 p-2">
-            <div className="p-2 text-2xl font-semibold">Nos adresses</div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
-              non consectetur quasi corporis ad illo blanditiis omnis,
-              exercitationem autem, impedit illum! Nulla voluptatem dignissimos
-              nesciunt provident, consectetur saepe mollitia.
-            </p>
-          </div>
-          <p className="bg-green-600 text-white p-4 rounded-r font-semibold">
-            Ouvert
-          </p>
-          <div className="rounded basis-11/12 border-2 border-black m-2 mr-0 p-2">
-            <div className="p-2 bg-white text-2xl font-semibold">
-              Nos adresses
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
-              non consectetur quasi corporis ad illo blanditiis omnis,
-              exercitationem autem, impedit illum! Nulla voluptatem dignissimos
-              nesciunt provident, consectetur saepe mollitia.
-            </p>
-          </div>
-          <p className="bg-red-600 text-white p-4 rounded-r font-semibold">
-            Fermé
-          </p>
+        <div>
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+          <Address />
         </div>
       </div>
       <Footer />
