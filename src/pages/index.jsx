b6547f1b97662fb337223729a6b28ad00ad3be4d
HugoWeh <hugo.wehrle@supdevinci-edu.fr>
2024-03-19 13:38:04 +0100

    chore: upgrde style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,16 +12,28 @@ const HomePage = () => {
           <ul className="bg-gray-200">
             <Nav />
           </ul>
-          <div className="justify-around border-t-2 border-black p-2 mb-2">
+          <div className="justify-around border-t-2 border-black p-2">
             <h1 className="text-2xl text-center">Avis</h1>
             <ul className="flex flex-row justify-around">
               <Stars />
             </ul>
           </div>
         </div>
-        <div className="flex flex-row w-5/6">
-          <div className="rounded border-2 border-black m-4 p-4">
-            <div className="p-2 text-center text-2xl font-semibold">
+        <div className="flex flex-wrap items-center w-full">
+          <div className="rounded basis-11/12 border-2 border-black m-2 mr-0 p-2">
+            <div className="p-2 text-2xl font-semibold">Nos adresses</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
+              non consectetur quasi corporis ad illo blanditiis omnis,
+              exercitationem autem, impedit illum! Nulla voluptatem dignissimos
+              nesciunt provident, consectetur saepe mollitia.
+            </p>
+          </div>
+          <p className="bg-green-600 text-white p-4 rounded-r font-semibold">
+            Ouvert
+          </p>
+          <div className="rounded basis-11/12 border-2 border-black m-2 mr-0 p-2">
+            <div className="p-2 bg-white text-2xl font-semibold">
               Nos adresses
             </div>
             <p>
@@ -31,28 +43,9 @@ const HomePage = () => {
               nesciunt provident, consectetur saepe mollitia.
             </p>
           </div>
-          <div className="rounded border-2 border-black m-4 p-4">
-            <div className="p-2 bg-white text-center text-2xl font-semibold">
-              Nos adresses
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
-              non consectetur quasi corporis ad illo blanditiis omnis,
-              exercitationem autem, impedit illum! Nulla voluptatem dignissimos
-              nesciunt provident, consectetur saepe mollitia.
-            </p>
-          </div>
-          <div className="rounded border-2 border-black m-4 p-4">
-            <div className="p-2 bg-white text-center text-2xl font-semibold">
-              Nos adresses
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odio
-              non consectetur quasi corporis ad illo blanditiis omnis,
-              exercitationem autem, impedit illum! Nulla voluptatem dignissimos
-              nesciunt provident, consectetur saepe mollitia.
-            </p>
-          </div>
+          <p className="bg-red-600 text-white p-4 rounded-r font-semibold">
+            Fermé
+          </p>
         </div>
       </div>
       <Footer />
