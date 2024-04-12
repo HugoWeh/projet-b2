0a775f8921b402c8d7411a266da9b3f55dc7f548
HugoWeh <hugo.wehrle@supdevinci-edu.fr>
2024-04-12 22:02:13 +0200

    modif

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,10 @@ export const Nav = () => (
   <>
     <div>
       {sites.map((value, index) => (
-        <li key={index} className="hover:border-l-4 hover:border-black">
+        <li
+          key={index}
+          className="hover:border-l-4 hover:border-black font-semibold"
+        >
           <button className="pl-2 py-2">{value}</button>
         </li>
       ))}
--- src/pages/api/address/[addressId].js
+++ src/pages/api/address/[addressId].js
@@ -1,10 +1,19 @@
 import { createRoute } from "@/api/createRoute"
 import { AddressModel } from "@/database/models/AddressModel"
 
-const handler = createRoute(async (req, res) => {const {AddressId = req.query const address = await AddressModel.findById(AddressId)}})
+const handler = createRoute(async (req, res) => {
+  const { AddressId } = req.query
+  const address = await AddressModel.findById(AddressId)
 
-if (!address) {
-  res.status(404).send({ error: "not found" })
-  
-  return
-}
+  if (!address) {
+    res.status(404).send({ error: "not found" })
+
+    return
+  }
+
+  if (req.method === "GET") {
+    res.send(address)
+  }
+})
+
+export default handler
